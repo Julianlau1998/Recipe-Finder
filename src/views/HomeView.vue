76c8ab379934b4829c5f45151fef8f3f78273d89
julianlau1998 <Julian.lau@aol.com>
2023-01-07 22:29:35 +0100

    Open category/country from favorites

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -47,10 +47,15 @@ export default {
   },
   created () {
     const page = localStorage.getItem('page')
-    if (page === null || page === undefined) {
+    if ((page === null || page === undefined) && !this.$route.query.category?.length && !this.$route.query.country?.length) {
       this.page = 0
+      console.log(1)
+    } else if (this.$route.query.category?.length || this.$route.query.country?.length) {
+      this.page = 1
+      console.log(2)
     } else {
       this.page = parseInt(page)
+      console.log(3)
     }
   },
   watch: {
